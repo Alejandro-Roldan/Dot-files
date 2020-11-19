35dd46ed373887e20bc71a2a6116612d01a21e0a
Alejandro Roldán <alej.roldan.trabajos@gmail.com>
2020-11-19 23:08:04 +0100

    Add files via upload

---
+++ launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar > /dev/null; do sleep 1; done
+
+# Launch bar
+# Bar is the name set in the polybar config, so if you change it, you have to change it here too.
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar --reload upper &
+done
+
+echo "Bars launched..."
